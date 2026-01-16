65d6cb83a092292ada5acaa105367096d5e9bda3
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-01-16 14:37:35 -0500

    I think I finished car class, and connect

---
+++ assignments/labs/lab_one/car.php
@@ -0,0 +1,22 @@
+class Car {
+    public string $make;
+    public string $model; 
+    public string $year; 
+    
+    public function __construct(string $make, string $model, string $year) {
+        $this->make = $make; 
+        $this->model = $model; 
+        $this->year = $year;
+    }
+
+    public function getBadge(): string {
+        $role = $this->isInstructor ? "Instructor" : "Student"; 
+        return "Name : {$this->name} | Age: {$this->age} | Role : $role";
+    }
+}
+
+//create an instance of the object 
+
+$car = new Car("Huyndai", "Elantra", "2025"); 
+
+echo $car->getBadge(); 
--- assignments/labs/lab_one/index.php
+++ assignments/labs/lab_one/index.php
@@ -1,4 +1,13 @@
 <?php 
-require "header.php"; 
+declare(strict_types=1); 
+require "connect.php";
+require "header.php";
+require "car.php"; 
 echo "<p> Follow the instructions outlined in instructions.txt to complete this lab. Good luck & have fun!😀 </p>";
 require "footer.php"; 
+
+/*
+
+Everything waas pretty good in terms of difficulty. The only part that I found challanging is setting up my github repo
+
+*/
