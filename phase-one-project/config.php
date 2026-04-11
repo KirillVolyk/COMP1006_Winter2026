f6028a13e89940ff67b0dcc2feac6c36935014f4
Kirill Volyk <200638948@student.georgianc.on.ca>
2026-04-10 23:40:35 -0400

    fixing pathin

--- phase-one-project/config.php
+++ phase-one-project/config.php
@@ -1,6 +1,8 @@
 <?php
 define('BASE_PATH', __DIR__);
-define('URL_ROOT', '/COMP1006_Winter2026/phase-one-project');
+//define('URL_ROOT', '/COMP1006_Winter2026/phase-one-project');
+define('URL_ROOT', '/~Kirill200638948/COMP1006_Winter2026/phase-one-project');
+
 
 define('INCLUDES', BASE_PATH . '/includes/');
 define('SECURITY', BASE_PATH . '/security/');
